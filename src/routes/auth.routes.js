d024f5b3439ef11fbba0a0d76b356c625016cfe8
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-06-21 02:14:47 +0300

    fix: updat user data validation mechanism

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -17,7 +17,7 @@ router.post(
   async (req, res) => {
     try {
       const errors = validationResult(req);
-      if (errors.isEmpty()) {
+      if (!errors.isEmpty()) {
         return res.status(400).json({
           errors: errors.array(),
           message: config.get('auth.incorrectData'),
@@ -53,7 +53,7 @@ router.post(
   async (req, res) => {
     try {
       const errors = validationResult(req);
-      if (errors.isEmpty()) {
+      if (!errors.isEmpty()) {
         return res.status(400).json({
           errors: errors.array(),
           message: config.get('auth.incorrectData'),
